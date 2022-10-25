3b120643ce1d44918c738a85f74d4854ff110d63
lgm1995 <rjsahr5045@gmail.com>
2022-10-25 17:41:38 +0900

    Feat: repository

---
+++ src/test/java/com/example/learnreacttypescript/LearnReactTypeScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnreacttypescript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnReactTypeScriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
