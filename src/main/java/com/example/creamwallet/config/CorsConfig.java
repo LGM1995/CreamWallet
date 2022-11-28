15a4635fb006f413589e623c4d17b505ad20a4e3
lgm1995 <rjsahr5045@gmail.com>
2022-11-28 17:02:30 +0900

    주석 추가

--- src/main/java/com/example/creamwallet/config/CorsConfig.java
+++ src/main/java/com/example/creamwallet/config/CorsConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
+    // Cors를 해결하는 방법으로 같은 패키지에 구성되어 있는 WebMvcConfig로도 해결이 가능하다.
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
