97ec6bda3a686a52e08f626371658bb3bd59a538
lgm1995 <rjsahr5045@gmail.com>
2022-12-01 14:52:56 +0900

    제네릭 필터에서 한번의 요청당 한번만 수행되는 OncePerRequestFilter로 변경

--- src/main/java/com/example/creamwallet/jwt/JwtFilter.java
+++ src/main/java/com/example/creamwallet/jwt/JwtFilter.java
@@ -1,46 +1,60 @@
 package com.example.creamwallet.jwt;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-public class JwtFilter extends GenericFilterBean {
+@RequiredArgsConstructor
+// 이전에는 GenericFilterBean 을 상속받아 doFilter를 오버라이드 하였으나 해당 필터는 서블렛이 작동전 과 작동 후 사이에
+// 다른 서블릿의 같은 필터 요청에 대해 필터가 다시 작동하는 현상이 생긴다.
+// 그러면 두번째 서블릿의 결과를 첫번째 서블릿도 받게 되는 현상으로 모든 서블릿의 요청당 한번만 수행하게 되는 OncePerRequestFilter를 사용하자.
+public class JwtFilter extends OncePerRequestFilter {
     private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
     public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
     private TokenProvider tokenProvider;
     public JwtFilter(TokenProvider tokenProvider) {
         this.tokenProvider = tokenProvider;
     }
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws IOException, ServletException {
+
+        // 1. Request Header 에서 토큰을 꺼냄
         String jwt = resolveToken(httpServletRequest);
+
+        // 2. validateToken 으로 유효성을 검사
         String requestURI = httpServletRequest.getRequestURI();
 
         if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
             Authentication authentication = tokenProvider.getAuthentication(jwt);
             SecurityContextHolder.getContext().setAuthentication(authentication);
+            // 로그에 토큰과 uri 정보를 저장
             logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
         } else {
+            // 로그에 오류 저장
             logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
         }
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 
+    // Request Header 에서 토큰 정보를 꺼냄
     private String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
             return bearerToken.substring(7);
         }
         return null;
