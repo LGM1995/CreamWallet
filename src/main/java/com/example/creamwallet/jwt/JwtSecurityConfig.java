c49f1c4358040937a1893ef5cb4a573e1fdac895
lgm1995 <rjsahr5045@gmail.com>
2022-12-02 22:54:15 +0900

    주석 추가

--- src/main/java/com/example/creamwallet/jwt/JwtSecurityConfig.java
+++ src/main/java/com/example/creamwallet/jwt/JwtSecurityConfig.java
@@ -1,16 +1,20 @@
 package com.example.creamwallet.jwt;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+// 직접 만든 TokenProvider 와 JwtFilter 를 SecurityConfig 에 적용할 때 사용
+@RequiredArgsConstructor
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
-    private TokenProvider tokenProvider;
+    private final TokenProvider tokenProvider;
 
-    public JwtSecurityConfig(TokenProvider tokenProvider) {
-        this.tokenProvider = tokenProvider;
-    }
+    //    @RequiredArgsConstructor(Lombok)의 사용으로 final 변수 생성자를 자동으로 만든다.
+    //    public JwtSecurityConfig(TokenProvider tokenProvider) {
+    //        this.tokenProvider = tokenProvider;
+    //    }
 
     @Override
     public void configure(HttpSecurity http) {
