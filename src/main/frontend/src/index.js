93485a4d6cc79fa69e4f59914ce7df71cf198e3b
lgm1995 <rjsahr5045@gmail.com>
2022-12-10 13:48:45 +0900

    index 수정

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -4,13 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
-import {store} from "./store";
-
+import {store} from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <Provider store={store}>
+  // <React.StrictMode> 해당코드는 개발 모드에서 두번 렌더링 과정을 거치는 검증으로 개발이 끝나고 나면 지워준다.
+    <Provider store={store}>  {/* 프로바이더를 App위로 감싸 App에게 store(저장소)를 사용할 수 있도록 전달한다. */}
       <App />
     </Provider>
   // </React.StrictMode>
